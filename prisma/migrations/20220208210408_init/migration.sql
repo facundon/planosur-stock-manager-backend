a1485bb88bd253859db1b45697a0928e9a905e61
Facundo Rodriguez <facundonrodriguez@gmail.com>
2022-02-08 18:35:42 -0300

    Create db schema

---
+++ prisma/migrations/20220208210408_init/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE `Product` (
+    `code` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `qty` INTEGER NOT NULL DEFAULT 1,
+    `unit` ENUM('g', 'kg', 'lt', 'ml') NOT NULL DEFAULT 'kg',
+    `providerId` INTEGER NOT NULL,
+    `categoryId` INTEGER NOT NULL,
+    `price` DECIMAL(8, 2) NOT NULL,
+
+    UNIQUE INDEX `Product_code_key`(`code`),
+    PRIMARY KEY (`code`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Provider` (
+    `id` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `phone` INTEGER NULL,
+    `address` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `Provider_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Category_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Product` ADD CONSTRAINT `Product_providerId_fkey` FOREIGN KEY (`providerId`) REFERENCES `Provider`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Product` ADD CONSTRAINT `Product_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
