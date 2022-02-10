3160f6c22bb4a4b20455520cf67ff5c94a5e0c1f
Facundo Rodriguez <facundonrodriguez@gmail.com>
2022-02-10 15:02:07 -0300

    Add helmet & passport packages

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
+import helmet from "helmet"
 import { HttpAdapterHost, NestFactory } from "@nestjs/core"
+import { NestExpressApplication } from "@nestjs/platform-express"
 import { PrismaClientExceptionFilter } from "src/prisma-client-exception.filter"
 import { PrismaQueryInterceptorInterceptor } from "src/prisma-query-interceptor.interceptor"
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
-   const app = await NestFactory.create(AppModule)
+   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+   app.disable("x-powered-by")
+   app.use(helmet())
 
    const { httpAdapter } = app.get(HttpAdapterHost)
    app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter))
+
    app.useGlobalInterceptors(new PrismaQueryInterceptorInterceptor())
 
    await app.listen(3001)
