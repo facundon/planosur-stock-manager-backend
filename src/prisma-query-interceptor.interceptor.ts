7af731464148b9cdeba7e1c48d83da794590dde6
Facundo Rodriguez <facundonrodriguez@gmail.com>
2022-02-10 16:28:36 -0300

    Fix types

--- src/prisma-query-interceptor.interceptor.ts
+++ src/prisma-query-interceptor.interceptor.ts
@@ -7,11 +7,12 @@ import {
    NestInterceptor,
 } from "@nestjs/common"
 import { Prisma } from "@prisma/client"
+import { Response } from "express"
 import { catchError, Observable } from "rxjs"
 
 @Injectable()
 export class PrismaQueryInterceptorInterceptor implements NestInterceptor {
-   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+   intercept(context: ExecutionContext, next: CallHandler): Observable<Response> {
       return next.handle().pipe(
          catchError(err => {
             if (err instanceof Prisma.PrismaClientValidationError) {
